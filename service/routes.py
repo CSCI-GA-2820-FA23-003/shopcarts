c3f3a35c65b96265f01f48246247bad259dbcdc8
Gary <garyhsu1209@gmail.com>
2023-10-14 21:11:08 +0000

    feat: api for creating a shopcart

--- service/routes.py
+++ service/routes.py
@@ -4,7 +4,7 @@ My Service
 Describe what your service does here
 """
 
-from flask import jsonify, request, url_for, abort
+from flask import jsonify, request, url_for, make_response
 from service.common import status  # HTTP Status Codes
 from service.models import Shopcart
 
@@ -29,3 +29,27 @@ def index():
 ######################################################################
 
 # Place your REST API code here ...
+
+######################################################################
+# CREATE A SHOPCART FOR A CUSTOMER
+######################################################################
+@app.route("/shopcarts/<int:customer_id>", methods=["POST"])
+def create_shopcart(customer_id):
+    """
+    Creates A SHOPCART FOR A CUSTOMER
+    """
+    app.logger.info("Request to create a shopcart")
+
+    shopcart = Shopcart()
+    
+    shopcart.customer_id = customer_id
+    
+    shopcart.create()
+
+    # Create a message to return
+    message = shopcart.serialize()
+    location_url = url_for("create_shopcart", customer_id=customer_id, _external=True)
+
+    return make_response(
+        jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+    )
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,10 +8,12 @@ Test cases can be run with the following:
 import os
 import logging
 from unittest import TestCase
+from tests.factories import ShopcartFactory, CartItemFactory
 from service import app
 from service.models import db
 from service.common import status  # HTTP Status Codes
 
+BASE_URL = "/shopcarts"
 
 ######################################################################
 #  T E S T   C A S E S
@@ -43,3 +45,16 @@ class TestYourResourceServer(TestCase):
         """ It should call the home page """
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+    
+    def test_create_account(self):
+        """It should Create a empty shopcart"""
+        shopcart = ShopcartFactory()
+        resp = self.client.post(
+            BASE_URL + "/" + str(shopcart.customer_id)
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # Check the data is correct
+        new_shopcart = resp.get_json()
+        self.assertEqual(new_shopcart["customer_id"], shopcart.customer_id, "customer_id does not match")
+        self.assertEqual(new_shopcart["items"], [], "Items does not match")
